a883c0fba154ad89ab4972cee3c289a653b35a5d
Amanda Pang <pang.am@northeastern.edu>
2025-11-22 15:40:42 -0500

    first commit

--- sphero/lib/python3.11/site-packages/spherov2/listeners/api_and_shell.py
+++
@@ -1,6 +0,0 @@
-from typing import NamedTuple
-
-
-class ApiProtocolVersion(NamedTuple):
-    major_version: int
-    minor_version: int
--- sphero/lib/python3.11/site-packages/spherov2/listeners/power.py
+++
@@ -1,7 +0,0 @@
-from typing import NamedTuple
-
-
-class BatteryVoltageStateThresholds(NamedTuple):
-    critical_threshold: float
-    low_threshold: float
-    hysteresis: float
