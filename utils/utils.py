eb338acace53608f2d9b90644df8cf0d475b475b
amanda-pang <amanda.pang@tufts.edu>
2025-12-03 11:55:35 -0500

    windows/mac sound update to utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -10,7 +10,12 @@ def speak(text):
     text_to_speak = text
     tts = gTTS(text=text_to_speak, lang='en')
     tts.save("speech.mp3")
+    
+    # macos
     os.system("afplay speech.mp3")
+    
+    # windows
+    # os.system('start "" "speech.mp3"')
 
 def get_random_color():
     return Color(r = np.random.randint(255), g = np.random.randint(255), b=np.random.randint(255))
